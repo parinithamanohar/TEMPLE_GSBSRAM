155d35dfca84e0959c792ce0e4a3f837088bb3a6
vikyathparrophins <vikyath@parrophins.com>
2023-03-03 12:51:13 +0530

    Overal changes

--- application/views/expenses/expenses.php
+++ application/views/expenses/expenses.php
@@ -312,6 +312,9 @@ if ($error) {
                                             <select class="form-control " id="year" name="year" required>
                                                 <option value="2022">2022</option>
                                                 <option value="2023">2023</option>
+                                                <option value="2024">2024</option>
+                                                <option value="2025">2025</option>
+                                                <option value="2026">2026</option>
                                             </select>
                                         </div>
                                     </div>
--- application/views/report/report.php
+++ application/views/report/report.php
@@ -438,6 +438,9 @@
                                 <select class="form-control required" id="" name="year">
                                     <option value="2022">2022</option>
                                     <option value="2023">2023</option>
+                                    <option value="2024">2024</option>
+                                    <option value="2025">2025</option>
+                                    <option value="2026">2026</option>
                                 </select>
                             </div>
                         </div>
