23335c0b71e1e31e96d6c02266100a76ebf5969c
parinithamanohar <parinitha@parrophins.com>
2023-03-06 13:03:01 +0530

    margin left changes donation reciept

--- application/views/DailyPooja/donationReceipt.php
+++ application/views/DailyPooja/donationReceipt.php
@@ -56,7 +56,7 @@ table {
             <table class="table text_highlight" style="background-color:#FFA500">
                 <tr>
                     <!-- <td style="text-align:center;" width="80">
-                        <!-- <img class="mt-2" width="100" height="90" src="<?php echo $companyLogo; ?>" alt="logo"> 
+                        <img class="mt-2" width="100" height="90" src="<?php echo $companyLogo; ?>" alt="logo"> 
                     </td> -->
                     <td width="700" style="text-align:center;">
                         <!-- <b style="font-size: 25px;margin-bottom: 2px;">ಶ್ರೀ ರಾಮ ಮಂದಿರ</b><br /> -->
@@ -175,7 +175,7 @@ table {
 
                         </table>
 
-                        <h4 style="margin-left:280px"> Collected By : <?php echo $donationInfo->type; ?></h4>
+                        <h4 style="margin-left:250px"> Collected By : <?php echo $donationInfo->type; ?></h4>
                         <br/>
                           
                 
